a95b501082a9b2d7dba480910cf0eb2529ec9906
bdmitrij1 <bdmitrij1@yandex.ru>
2021-11-15 17:56:33 +0300

    Дописываетл функцию вывода картинок со случайными данными

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-import {createPicture} from './utils/create-picture.js';
+import {generatePictureData} from './utils/create-picture.js';
 import {renderingPicture} from './utils/rendering-picture.js';
 
-// временно вызвал функцию, чтобы убрать ошибку eslint
-renderingPicture(createPicture());
-
-
+renderingPicture(generatePictureData(11));
--- js/utils/rendering-picture.js
+++ js/utils/rendering-picture.js
@@ -1,20 +1,21 @@
-// import {createPicture} from './create-picture.js';
-
 const pictures = document.querySelector('.pictures');
 const fragment = document.createDocumentFragment();
 
-const renderingPicture = function (imageData) {
-  const elementFragment = document.querySelector('#picture').content;
-  const imgPicture = elementFragment.querySelector('.picture__img');
-  imgPicture.src = imageData.url;
+const renderingPicture = function (imagesData) {
+  imagesData.forEach((imageData) => {
+    const elementFragment = document.querySelector('#picture').content.cloneNode(true);
+    const imgPicture = elementFragment.querySelector('.picture__img');
+    imgPicture.src = imageData.url;
 
-  const commentsPicture = elementFragment.querySelector('.picture__comments');
-  commentsPicture.textContent = imageData.comments.id;
+    const commentsPicture = elementFragment.querySelector('.picture__comments');
+    commentsPicture.textContent = imageData.comments.length;
 
-  const likesPicture = elementFragment.querySelector('.picture__likes');
-  likesPicture.textContent = imageData.likes;
+    const likesPicture = elementFragment.querySelector('.picture__likes');
+    likesPicture.textContent = imageData.likes;
 
-  fragment.appendChild(elementFragment);
+    fragment.appendChild(elementFragment);
+
+  });
   pictures.appendChild(fragment);
 };
 
